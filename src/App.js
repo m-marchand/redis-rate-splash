cda250b5eb446fb9a53d9c23c76acd6c699d7e9b
Matthew <matthew.marchand.93@gmail.com>
2021-02-02 19:11:42 -0800

    initialize redisrate splash page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './redisrate.png';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>RedisRate is an analytical tool to monitor<br></br> pertinent Redis Performance metrics</h2>
+        <h4>Built for developers, by developers</h4>
+        <Button variant='outlined'>Get Started</Button>
       </header>
+      <div>
+        SCREENSHOTS
+      </div>
+      <div>
+        THE TEAM
+      </div>
     </div>
   );
 }
